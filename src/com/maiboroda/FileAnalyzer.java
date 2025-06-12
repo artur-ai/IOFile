4eb9fc2cdcae2b094437b931201eb427100c81df
artur <mauboroda.arturtu@gmail.com>
2025-06-12 16:49:29 +0300

    Improve code and create new branch -create new branch - enhancement/code-improvements -divide FileAnalyzer for few methods -add Result.java -add StartForUser.java -improve pom.xml, add new update code -add clean .gitignore

---
+++ src/com/maiboroda/FileAnalyzer.java
@@ -0,0 +1,76 @@
+package com.maiboroda;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileAnalyzer {
+    public Result analyze(String path, String word) {
+        validatePath(path);
+        validateWord(word);
+        String text = readPath(path);
+        List<String> sentences = splitToSentences(text);
+        List<String> serchedSentences = searchWordInSentences(sentences, word);
+        int count = countWord(serchedSentences, word);
+        return new Result(word, serchedSentences, count);
+    }
+
+    private void validateWord(String word) {
+        if (word == null || word.length() == 0) {
+            throw new IllegalArgumentException("Word cannot be null or empty");
+        }
+    }
+
+    private void validatePath(String path) {
+        if (path == null || path.length() == 0) {
+            throw new IllegalArgumentException("Path cannot be null or empty");
+        }
+    }
+
+    private String readPath(String path) {
+        try {
+            return Files.readString(Paths.get(path));
+        } catch (IOException exception) {
+            System.out.println("No such file");
+            exception.printStackTrace();
+            return "";
+        }
+    }
+
+    private List<String> splitToSentences(String text) {
+        String[] sentences = text.split("[!?.]");
+        List<String> list = new ArrayList<>(List.of(sentences));
+        return list;
+    }
+
+    private List<String> searchWordInSentences(List<String> sentences, String word) {
+        List<String> result = new ArrayList<>();
+        String lowerCaseWord = word.toLowerCase();
+        for (String sentence : sentences) {
+            String[] words = sentence.toLowerCase().split("\\W+");
+            for (String searchWord : words) {
+                if (searchWord.equals(lowerCaseWord)) {
+                    result.add(sentence.trim() + ".");
+                }
+            }
+
+        }
+        return result;
+    }
+
+    private int countWord(List<String> searchedSentences, String word) {
+        int count = 0;
+        String lowerCaseWord = word.toLowerCase();
+        for (String searchedSentence : searchedSentences) {
+            String[] words = searchedSentence.toLowerCase().split("\\W+");
+            for (String searchedWord : words) {
+                if (searchedWord.equals(lowerCaseWord)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
---
+++ src/com/maiboroda/StartForUser.java
@@ -0,0 +1,27 @@
+package com.maiboroda;
+public class StartForUser {
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Arguments must be two!");
+        }
+
+        String path = args[0];
+        String word = args[1];
+        FileAnalyzer fileAnalyzer = new FileAnalyzer();
+
+
+        try {
+            Result result = fileAnalyzer.analyze(path, word);
+            System.out.println("Word: " + result.getWord());
+            System.out.println("Count in file: " + result.getCount());
+            System.out.println("Sentances with word " + result.getWord() + ": ");
+            for (String sentence : result.getSentences()){
+                System.out.println("-" + sentence);
+            }
+        } catch (IllegalArgumentException exception){
+            exception.printStackTrace();
+        }
+    }
+
+
+}
